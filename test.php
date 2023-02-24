2c0f3f7f5569d4d35522555a5e3a95c8e8a7b061
king-kand <stevenokanda@gmail.com>
2023-02-24 18:36:00 +0300

    kindly fisish

---
+++ test.php
@@ -0,0 +1,27 @@
+<!-- 
+<?
+// $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
+// curl_setopt($ch, CURLOPT_HTTPHEADER, [
+//     'Authorization: Bearer BkvkxGXftQyCRRSv5ttlgkyOGVjl',
+//     'Content-Type: application/json'
+// ]);
+// curl_setopt($ch, CURLOPT_POST, 1);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, {
+//     "BusinessShortCode": 174379,
+//     "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjMwMjE5MTcyNDI5",
+//     "Timestamp": "20230219172429",
+//     "TransactionType": "CustomerPayBillOnline",
+//     "Amount": 100,
+//     "PartyA": 254759626677,
+//     "PartyB": 174379,
+//     "PhoneNumber": 254759626677,
+//     "CallBackURL": "https://mydomain.com/path",
+//     "AccountReference": "",
+//     "TransactionDesc": "SIt 310" 
+//   });
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// $response     = curl_exec($ch);
+// curl_close($ch);
+// echo $response;
+
+?> -->
